c927171b729c0e7d30bcb2496f03b82870534d3d
Estevan Maito <ejmaito@gmail.com>
2020-07-04 01:02:59 -0300

    move banner down

--- components/Banner.js
+++ components/Banner.js
@@ -4,16 +4,16 @@ import Container from './layout/Container'
 function Banner({ children, tag, href, ...other }) {
   const Component = tag || 'a'
   return (
-    <Component href={href}>
-      <a className="block py-2 font-mono bg-primary" {...other}>
-        <Container>
+    <Container className="fixed inset-x-0 bottom-0">
+      <Component href={href}>
+        <a className="block px-4 py-3 mx-auto mb-4 font-mono shadow-md bg-primary" {...other}>
           <div className="flex justify-between">
             {children}
             <span>Read more</span>
           </div>
-        </Container>
-      </a>
-    </Component>
+        </a>
+      </Component>
+    </Container>
   )
 }
 
