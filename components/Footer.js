219d42a4e3bc554455ebae76cb2ad4790a4f234e
Estevan Maito <ejmaito@gmail.com>
2020-07-05 16:47:07 -0300

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from '../components/layout/Container'
+
+function Footer() {
+  return (
+    <footer className="my-20 font-mono">
+      <Container>
+        <p className="text-sm text-center text-gray-800">
+          Made by{' '}
+          <a className="border-b border-primary" href="https://twitter.com/estevanmaito">
+            Estevan Maito
+          </a>
+        </p>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
