26ee82a2e4ab7a79b052aadcc04c9f02292eab2a
Estevan Maito <ejmaito@gmail.com>
2020-07-11 22:08:03 -0300

    add backdrop docs page

---
+++ components/BackdropExample.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+import { Backdrop, Button } from 'windmill-react-ui'
+
+function DropdownExample() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function toggleBackdrop() {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className="relative">
+      <Button onClick={toggleBackdrop}>Open backdrop</Button>
+
+      {isOpen && <Backdrop onClick={toggleBackdrop} />}
+    </div>
+  )
+}
+
+export default DropdownExample
