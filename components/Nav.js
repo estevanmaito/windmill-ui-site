c4e518ba6fea9a2650ada0fc5c86913c01135874
Estevan Maito <ejmaito@gmail.com>
2020-07-11 22:43:51 -0300

    add mobile menu

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { Backdrop } from 'windmill-react-ui'
 import Container from './layout/Container'
+import MenuIcon from '../icons/menu.svg'
 
-function Nav() {
+function Nav({ menu }) {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <Container className="py-4">
-      <Link href="/">
-        <a className="block font-mono font-semibold text-right text-gray-700">windmill ui</a>
-      </Link>
+      {menu && (
+        <>
+          <div className="flex items-center justify-between -mt-1 md:hidden">
+            <button onClick={() => setIsOpen(!isOpen)} className="p-1 -ml-1">
+              <MenuIcon className="w-6 h-6" />
+            </button>
+            {isOpen && <Backdrop onClick={() => setIsOpen(false)}>{menu}</Backdrop>}
+            <Link href="/">
+              <a className="block font-mono font-semibold text-right text-gray-700">windmill ui</a>
+            </Link>
+          </div>
+          <Link href="/">
+            <a className="hidden font-mono font-semibold text-right text-gray-700 md:block">
+              windmill ui
+            </a>
+          </Link>
+        </>
+      )}
     </Container>
   )
 }
--- layouts/docs.js
+++ layouts/docs.js
@@ -9,6 +9,59 @@ import components from '../pages.json'
 export default (frontMatter) => {
   return ({ children }) => {
     const router = useRouter()
+
+    const mobileMenu = (
+      <aside className="absolute inset-y-0 left-0 z-10 w-48 p-4 overflow-x-auto bg-white shadow-md md:hidden">
+        <ul className="mb-8 space-y-2 text-gray-500">
+          {/* refactor this ASAP! */}
+          <li
+            className={`${
+              router.pathname.split('/')[2] === 'installation' && 'text-gray-700 font-semibold'
+            }`}
+          >
+            <Link href="/react-ui/installation">
+              <a>Installation</a>
+            </Link>
+          </li>
+          <li
+            className={`${
+              router.pathname.split('/')[2] === 'customization' && 'text-gray-700 font-semibold'
+            }`}
+          >
+            <Link href="/react-ui/customization">
+              <a>Customization</a>
+            </Link>
+          </li>
+          <li
+            className={`${
+              router.pathname.split('/')[2] === 'dark-theme' && 'text-gray-700 font-semibold'
+            }`}
+          >
+            <Link href="/react-ui/dark-theme">
+              <a>Dark theme</a>
+            </Link>
+          </li>
+        </ul>
+        <span className="block mb-2 font-mono text-sm font-semibold tracking-wide text-gray-500 uppercase">
+          Components
+        </span>
+        <ul className="space-y-2 text-gray-500">
+          {components.map((page) => (
+            <li
+              key={page.url}
+              className={`${
+                router.pathname.split('/')[3] === page.url && 'text-gray-700 font-semibold'
+              }`}
+            >
+              <Link href={`/react-ui/components/${page.url}`}>
+                <a>{page.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
+    )
+
     return (
       <>
         <style
@@ -22,7 +75,7 @@ export default (frontMatter) => {
           }}
         />
         <div className="bg-pattern">
-          <Nav />
+          <Nav menu={mobileMenu} />
           <Container>
             <header className="max-w-2xl py-20 text-gray-700">
               <h1 className="text-5xl font-extrabold leading-none tracking-tight font-montserrat md:text-6xl">
@@ -34,7 +87,7 @@ export default (frontMatter) => {
         </div>
         <Container className="my-16">
           <div className="grid grid-cols-12 gap-8 -mt-8">
-            <aside className="col-span-2">
+            <aside className="hidden col-span-2 md:block">
               <div className="sticky top-0 pt-8">
                 <ul className="mb-8 space-y-2 text-gray-500">
                   {/* refactor this ASAP! */}
@@ -88,8 +141,10 @@ export default (frontMatter) => {
                 </ul>
               </div>
             </aside>
-            <main className="col-span-8 pt-8 markdown">{children}</main>
-            <aside className="col-span-2">
+            <main className="col-span-12 pt-8 md:col-span-10 lg:col-span-8 markdown">
+              {children}
+            </main>
+            <aside className="hidden col-span-2 lg:block">
               <div className="sticky top-0 pt-8">
                 <span className="block mb-2 font-mono text-sm font-semibold tracking-wide text-gray-500 uppercase">
                   On this page
