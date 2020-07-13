84aad529d329f0d2d7680e4801e4353ca5671222
Estevan Maito <ejmaito@gmail.com>
2020-07-13 15:49:13 -0300

    add transition docs page

---
+++ components/TransitionExample.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+import { Transition, Button } from 'windmill-react-ui'
+
+function TransitionExample() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="relative">
+      <Button onClick={() => setIsOpen(!isOpen)}>Open dropdown</Button>
+
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <div className="absolute left-0 w-56 mt-2 rounded-md shadow-lg">
+          <div className="p-4 bg-white rounded-md shadow-xs">Hi</div>
+        </div>
+      </Transition>
+    </div>
+  )
+}
+
+export default TransitionExample
