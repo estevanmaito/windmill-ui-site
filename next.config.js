b9be3145b910f62093ea559604bc9fac13dfdc23
Estevan Maito <ejmaito@gmail.com>
2020-07-15 15:18:05 -0300

    add bundle analyze

--- next.config.js
+++ next.config.js
@@ -1,34 +1,39 @@
 const withMdxEnhanced = require('next-mdx-enhanced')
 const withReactSvg = require('next-react-svg')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 const path = require('path')
 
-module.exports = withMdxEnhanced({
-  layoutPath: 'layouts',
-  fileExtensions: ['mdx'],
-  remarkPlugins: [],
-  rehypePlugins: [require('rehype-slug'), require('rehype-autolink-headings')],
-  extendFrontMatter: {
-    process: async (mdxContent, frontMatter) => {
-      const innerLinks = []
+module.exports = withBundleAnalyzer(
+  withMdxEnhanced({
+    layoutPath: 'layouts',
+    fileExtensions: ['mdx'],
+    remarkPlugins: [],
+    rehypePlugins: [require('rehype-slug'), require('rehype-autolink-headings')],
+    extendFrontMatter: {
+      process: async (mdxContent, frontMatter) => {
+        const innerLinks = []
 
-      const matches = mdxContent.matchAll(/##\s(.*)/g)
+        const matches = mdxContent.matchAll(/##\s(.*)/g)
 
-      for (const match of matches) {
-        innerLinks.push({
-          title: match[1],
-          url: `#${match[1].split(' ').join('-').toLowerCase()}`,
-        })
-      }
+        for (const match of matches) {
+          innerLinks.push({
+            title: match[1],
+            url: `#${match[1].split(' ').join('-').toLowerCase()}`,
+          })
+        }
 
-      return { innerLinks }
+        return { innerLinks }
+      },
+      phase: 'both',
     },
-    phase: 'both',
-  },
-})(
-  withReactSvg({
-    include: path.resolve(__dirname, 'icons'),
-    webpack(config, options) {
-      return config
-    },
-  })
+  })(
+    withReactSvg({
+      include: path.resolve(__dirname, 'icons'),
+      webpack(config, options) {
+        return config
+      },
+    })
+  )
 )
