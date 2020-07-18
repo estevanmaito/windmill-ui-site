40fd86755a9af0b1590641ddc423cae79ec3a953
Estevan Maito <ejmaito@gmail.com>
2020-07-18 18:34:48 -0300

    center text on outline button

--- components/ButtonOutline.js
+++ components/ButtonOutline.js
@@ -4,7 +4,7 @@ function ButtonOutline({ children, tag, className, ...other }) {
   const Component = tag || 'button'
   return (
     <Component
-      className={`inline-block px-6 py-2 font-mono text-gray-700 border border-primary focus:outline-none ${
+      className={`inline-block text-center px-6 py-2 font-mono text-gray-700 border border-primary focus:outline-none ${
         className || ''
       }`}
       {...other}
